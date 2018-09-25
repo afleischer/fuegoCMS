7d5d4ced5841126af0dc721a4465eb096fedfd91
afleischer <afleischer1337@gmail.com>
2018-09-25 00:40:12 -0700

    Updated GhostElement to fix attribute non-definition errors.  Minor style updates.

--- components/SIDEBAR_Add_To_Page/VisualEditorChildren/GhostElement.js
+++ components/SIDEBAR_Add_To_Page/VisualEditorChildren/GhostElement.js
@@ -37,10 +37,28 @@ ghostFunction(){
         let uniqueKeyIterator = uniqueKeysArray[i];
         let child_base = snapshot.child(currPage).child("tags").child(uniqueKeyIterator).val()
         let tag_type = child_base.tag_type;
-        let tag_content = child_base.content;
+        //let tag_content = child_base.content;
+        let TagContent = child_base.content;
         let tag_placement = child_base.placement;
-        let tag_style = child_base.style;
+        //let tag_style = child_base.style;
+        
+        try{
+           let TagAttributes = child_base.attributes.toLowerCase();
+        }catch(error){
+          let TagAttributes = null;
+        }
+        
+        let TagName = tag_type.toLowerCase();
 
+        if(TagAttributes != undefined){
+            pageTags.push(<TagName {...TagAttributes}>{TagContent}</TagName>);  
+        }else {
+           pageTags.push(<TagName >{TagContent}</TagName>);  
+        }
+
+        
+
+        /* 
         if (tag_type == 'p'){
           pageTags.push(<p styles = {tag_style}>{tag_content}</p>);
         }else if (tag_type == 'h1'){
@@ -49,6 +67,8 @@ ghostFunction(){
         else if(tag_type == 'img'){
             pageTags.push(<img styles = {tag_style} src = {tag_content}></img>);
           } 
+
+        */
 
       }
         return pageTags;
