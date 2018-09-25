33562ae9f1478338d580439373968e5e67f1a391
afleischer <afleischer1337@gmail.com>
2018-09-25 09:29:03 -0700

    created new branch for the attribute selector functionality

---
+++ components/SIDEBAR_Change_Attributes/sidebar_list_children/AttributeSelector.js
@@ -0,0 +1,7 @@
+
+
+/*===================
+**
+**  When the user selects an element within the iFrame, we will list the properties and give the user an option to modify them. 
+**
+==================*/
