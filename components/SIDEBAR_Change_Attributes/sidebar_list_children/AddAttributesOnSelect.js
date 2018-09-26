4a2db51f90f39ba5c00e1ea4191150d47227f9d5
afleischer <afleischer1337@gmail.com>
2018-09-26 11:57:47 -0700

    Added ability for multiple lines

--- components/SIDEBAR_Change_Attributes/sidebar_list_children/AddAttributesOnSelect.js
+++ components/SIDEBAR_Change_Attributes/sidebar_list_children/AddAttributesOnSelect.js
@@ -7,6 +7,13 @@ class AddAttributesOnSelect extends React.Component{
 
 		this.AttributeSelectorList = this.AttributeSelectorList.bind(this);
 		this.RemoveAttribue = this.RemoveAttribue.bind(this);
+		this.UpdateAttribute = this.UpdateAttribute.bind(this);
+		this.AddAttribute = this.AddAttribute.bind(this);
+		this.AdditionalAttributes = this.AdditionalAttributes.bind(this);
+	}
+
+	state = {
+		attributeLines : 0
 	}
 
 
@@ -41,12 +48,18 @@ Begin functions
 					<div>
 						<label for={attribute_array[i]}>{attribute_array[i].name}</label>
 						<input onChange ={(e) => this.UpdateAttribute(e)} type = "name" name = {attribute_array[i].value} value = {attribute_array[i].value} className = "tag-name-change-attribute" />
-						<div onClick = {(e) => this.RemoveAttribue} className = "remove_attribute">X</div> 
+						<button onClick = {(e) => this.RemoveAttribue(e)} className = "remove_attribute">Remove Attribute</button> 
 					</div>
 					);
 			}
 		}
-		return(<div className = "attribute-selector-sub-container">{returnArray}</div>);
+		return(<div className = "attribute-selector-sub-container">{returnArray}
+			<input name="AddAttribute" onClick = {(e) => {this.AddAttribute(e)}} />
+			{this.AdditionalAttributes()}
+			 <button onClick = {(e) => this.setState({attributeLines : this.state.attributeLines + 1}) }>Add new attribute</button> 
+			 <button onClick = {(e) => this.setState({attributeLines : this.state.attributeLines - 1}) }> Remove attribute line</button>
+			}
+			</div>);
 	}	
 
 	RemoveAttribue(event){
@@ -66,6 +79,8 @@ Begin functions
 			}
 
 		}
+
+		toRemove.removeChild();
 	}
 
 	UpdateAttribute(){
@@ -92,6 +107,37 @@ Begin functions
 		}
 	}
 
+	AddAttribute(){
+		var pageURL = this.props.currentPage;
+		var toUpdate = event.target.previousSibling.name;
+		var targetData = event.target;
+		const RefToLoop = firebase.database().ref('pages/'+pageURL+"/tags/");
+		var RefBase = firebase.database().ref('pages/'+pageURL+"/tags/")
+
+
+		for(child in RefToLoop){
+			if(child.attributes != undefined){
+				var childKey = Object.keys(child);
+
+				if(child.attributes === toUpdate){
+					//remove from firebase
+					let updateRef = firebase.database().ref('pages/'+pageURL+"/tags/"+childKey+"/attributes/"+toUpdate);
+					var updates = {
+						[toUpdate] : targetData.value
+					}
+					updateRef.update(updates);
+				}
+			}
+		}
+	}
+
+	AdditionalAttributes(){
+		var returnArray = []
+		for(let i = 0; i < this.state.attributeLines; i++){
+			returnArray.push(<input name="AddAttribute" onClick = {(e) => {this.AddAttribute(e)}} />)
+		}
+		return returnArray;
+	}
 
 	ComponentDidUpdate(){
 		AttributeSelectorList();
